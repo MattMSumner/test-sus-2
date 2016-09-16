c845d915644b0a3f4edd694ba4fe37a3a2f4b489
Matthew Sumner <matt.m.sumner@gmail.com>
2016-09-16 10:18:21 -0400

    High score

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160916140704) do
+ActiveRecord::Schema.define(version: 20160916140802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 20160916140704) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+  end
+
+  create_table "high_scores", force: :cascade do |t|
+    t.string   "game"
+    t.integer  "score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,2 +1,6 @@
 FactoryGirl.define do
+  factory :high_score do
+    game "MyString"
+    score 1
+  end
 end
